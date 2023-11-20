d36fa1a5248bf9588792336caf0991b00fe09681
1samicaro <smchavarriar@correo.udistrital.edu.co>
2023-11-19 23:20:16 -0500

    Logo BA

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,7 +1,7 @@
 import banner1 from "../../assets/banner1.jpg";
 import banner2 from "../../assets/banner2.jpg";
 import banner3 from "../../assets/banner3.jpg";
-import logo from "../../assets/logoleo.png";
+import logo from "../../assets/logoba.png";
 import { Link } from "react-router-dom";
 import "./Home.css"
 
